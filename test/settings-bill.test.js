29277c9e20bbc6d1c40ef18cc04aca70fff02ae7
Thandeka93 <noluthandombali4@gmail.com>
2023-08-03 13:27:38 +0200

    test passing

--- test/settings-bill.test.js
+++ test/settings-bill.test.js
@@ -45,3 +45,42 @@ describe("Warning and Critical level", function(){
 
     })
 })
+
+describe("The totals function", function(){
+    it("It should be able to return total for calls at R2.50 each" , function(){
+        let bill = settingsBill();
+        bill.setCallCost(2.50)
+        bill.setSmsCost(0.70)
+
+
+        bill.makeCall();
+        bill.makeCall();
+        bill.makeCall();
+        bill.makeCall();
+
+        assert.equal(10.0, bill.getTotalCost())
+        assert.equal(10.0, bill.getTotalCallCost())
+        assert.equal(0.00, bill.getTotalSmsCost())
+
+    })
+
+    
+    it("It should be able to return total for sms at R 0.65 each" , function(){
+        let bill = settingsBill();
+        bill.setCallCost(0)
+        bill.setSmsCost(0.65)
+
+
+        bill.makeCall();
+        bill.makeCall();
+        bill.makeCall();
+        bill.sendSms();
+        bill.sendSms();
+
+        assert.equal(1.30, bill.getTotalCost())
+        assert.equal(0.00, bill.getTotalCallCost())
+        assert.equal(1.30, bill.getTotalSmsCost())
+
+    });
+})
+
